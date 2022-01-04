0043e4d7767b89a2a7cb099df05748553b568049
hilal <62089994+ayperi-hilal@users.noreply.github.com>
2022-01-04 09:17:52 +0300

    Add files via upload

---
+++ personelisTakip/personelisTakip/haberler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace personelisTakip
+{
+    public partial class haberler : Form
+    {
+        public haberler()
+        {
+            InitializeComponent();
+        }
+    }
+}
